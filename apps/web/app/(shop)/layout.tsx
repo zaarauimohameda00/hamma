72b81bd9e069d104f5ab5ccd2fd9efba1282cde9
zaarauimohameda00 <zaarauimohamedamine9@gmail.com>
2025-08-22 10:22:55 +0100

    Revert "Build production-ready e-commerce app with Supabase"

--- apps/web/app/(shop)/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function ShopLayout({ children }: { children: ReactNode }) {
-  return <div className="container mx-auto p-4">{children}</div>;
-}
